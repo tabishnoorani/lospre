b0f6c6832572a01f20e2bba385f656d857ef0ddb
Tabbish Noorani <tabishnoornai@gmail.com>
2017-12-22 00:23:55 +1000

    Express and EJS setup

---
+++ server.js
@@ -0,0 +1,25 @@
+import config, { nodeEnv } from './config';
+import express from 'express';
+import api from './api';
+
+
+const server = express();
+
+server.set ('view engine', 'ejs'); //it will look for ejs file in views folder.
+
+server.get('/', (req, res)=>{
+    res.send ('Hello from server Root');
+});
+
+server.get('/ejs', (req, res)=>{
+    res.render ('index',{content: 'Hello from EJS'});
+});
+
+
+server.use('/api', api);
+server.use(express.static('public'));
+
+
+server.listen(config.port, (err)=>{
+    console.log (err || ("The server is running on port:" + config.port)); 
+});
