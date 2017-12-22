5b440405b9a1703ea81cffa6520e83e8140ab0f5
Tabbish Noorani <tabishnoornai@gmail.com>
2017-12-22 19:39:33 +1000

    added SCSS Node Middleware

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 import config, { nodeEnv } from './config';
 import express from 'express';
 import api from './api';
-
+import sassMiddleware from 'node-sass-middleware';
+import path from 'path';
 
 const server = express();
+
+server.use (sassMiddleware({
+    src: path.join(__dirname, 'sass'),
+    dest: path.join(__dirname, 'public')
+}));
 
 server.set ('view engine', 'ejs'); //it will look for ejs file in views folder.
 
 server.get('/', (req, res)=>{
     res.render ('index',{content: 'Hello from EJS'});
 });
-
 
 server.use('/api', api);
 server.use(express.static('public'));
